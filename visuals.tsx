1deb1d06264e76b196d45cee99431ae633d132ae
Artem Barmin <artem.barmin@gmail.com>
2022-04-29 10:36:05 +0300

    Matrix view

--- visuals.tsx
+++ visuals.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import _ from "lodash";
 
 interface BreadcrumbsProps {
     file: string;
@@ -14,5 +15,49 @@ export function SimplePath({ relations }: BreadcrumbsProps) {
                 <div key={i}> {path.join(" -> ")}</div>
             ))}
         </div>
+    );
+}
+
+export function Matrix({ relations }: BreadcrumbsProps) {
+    const rows = relations.length;
+    const cols = _.maxBy(relations, (path) => path.length)?.length || 0;
+
+    const transformed = relations.map((row, rowNum) =>
+        row.map((value) => ({ value, rowspan: 1, rowNum }))
+    );
+
+    _.times(cols, (col) =>
+        _.times(rows, (row) => {
+            const cell = transformed[row][col];
+            const prevCell = row > 0 && transformed[row - 1][col];
+            if (row > 0 && cell && prevCell && cell.value === prevCell.value) {
+                transformed[row][col] = prevCell;
+                prevCell.rowspan++;
+            }
+        })
+    );
+
+    const style = {
+        border: "1px solid black",
+        borderCollapse: "collapse",
+    };
+
+    return (
+        <table style={{ ...style, width: "100%" }}>
+            {_.times(rows, (row) => (
+                <tr>
+                    {_.times(cols, (col) => {
+                        const cell = transformed[row][col];
+                        if (!cell) return null;
+                        if (cell.rowNum == row)
+                            return (
+                                <td style={style} rowSpan={cell.rowspan}>
+                                    {cell.value}
+                                </td>
+                            );
+                    })}
+                </tr>
+            ))}
+        </table>
     );
 }
